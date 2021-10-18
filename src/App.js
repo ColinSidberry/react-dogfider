46dda1c6b51c8c88086320e99c28c34582724494
Colin Sidberry <colinsidberry@gmail.com>
2021-10-18 11:52:10 -0700

    drafted app component. toggleIsLoaded is still broken.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import './App.css';
+import { useState } from "react";
+import Routes from "./Routes";
+import NavBar from "./NavBar";
+import { BrowserRouter, NavLink } from 'react-router-dom';
 
+/**FIXME 
+ * Toggle between loading page
+ * and then show dog list when data is recieved
+ * 
+ * state: isLoaded
+*/
 function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  function toggleIsLoaded(paramToBeDecided) {
+    //some condition that checks the ajax
+      //if promise reolved
+        //setIsLoaded(true);
+  }
   return (
-    <div className="App">
-      <h1>Good luck!</h1>
+    <div>
+      <BrowserRouter>
+        <NavBar />
+        <Routes isLoaded={isLoaded} toggleIsLoaded={toggleIsLoaded} />
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+/**
+ * App
+    * state: isLoaded
+ *  Routes
+ *    /Dogs
+ *
+ *     /Dogs/:name
+ */
